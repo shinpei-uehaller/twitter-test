c9aebf1e3902f88e1d1c47395377552915481521
上原 新平 <shinpei.uehara@gmail.com>
2014-09-12 17:48:04 +0900

    twiiter風

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(:username => params[:username])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/new'
+
+  post "tweets" => "tweets#create"
+
   get 'users/index'
 
-  get 'users/show'
+  get 'users/show/:username' => 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
